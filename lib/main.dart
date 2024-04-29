5a0408ca6d71895e380ce345aba67d51665cedc6
parcidio <parcidioandre@gmail.com>
2024-04-29 23:17:34 +0200

    implemented the map

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,35 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:latlong2/latlong.dart';
 
 void main() {
   runApp(const MainApp());
 }
 
 class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+  const MainApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+    return MaterialApp(
+      home: Directionality(
+        textDirection: TextDirection.ltr,
+        child: Scaffold(
+          
+          body: content(),
         ),
       ),
     );
   }
+  
+  content() {
+    return FlutterMap(options:    MapOptions(
+      initialCenter: LatLng(-8.8, 13.2),
+      initialZoom: 2.5,
+    ), children: [openStreetMap]);
+  }
 }
+TileLayer get openStreetMap => TileLayer(
+  urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
+  userAgentPackageName: 'com.example.app',
+);
